75d6e814911f78aaf7d57809da148a71d61e516d
qqw <qqw@uplink.net.cn>
2019-04-26 15:16:53 +0800

    dsa

--- pages/store/store.js
+++ pages/store/store.js
@@ -4,25 +4,50 @@ const lumos = require('../../utils/lumos.minprogram.js')
 const app = getApp()
 
 Page({
-  data: {
-  },
-  onLoad: function (options) {
+  data: {},
+  onLoad: function(options) {
     var _this = this
-    lumos.getJson({
-      url: config.apiUrl.storeList, urlParams: {
-        merchantId: "",
-        lat: 0,
-        lng: 0
-      },
-      success: function (res) {
-        _this.setData({
-          list: res.data,
-          currentStore: ownRequest.getCurrentStore()
-        })
+
+
+    function getStores(lat, lng) {
+      lumos.getJson({
+        url: config.apiUrl.storeList,
+        urlParams: {
+          merchantId: "",
+          lat: lat,
+          lng: lng
+        },
+        success: function (res) {
+          _this.setData({
+            list: res.data,
+            currentStore: ownRequest.getCurrentStore()
+          })
+        }
+      })
+    }
+
+    wx.getLocation({
+      type: 'wgs84',
+      success: function(res) {
+        // console.log(res);
+        var latitude = res.latitude
+        var longitude = res.longitude
+
+
+        getStores(latitude, longitude)
+
+        //弹框
+        // wx.showModal({
+        //   title: '当前位置',
+        //   content: "纬度:" + latitude + ",经度:" + longitude,
+        // })
       }
     })
+
+
+    getStores(0,0)
   },
-  selectStore: function (e) {
+  selectStore: function(e) {
     var store = e.currentTarget.dataset.replyStore
     ownRequest.setCurrentStore(store);
     wx.reLaunch({
